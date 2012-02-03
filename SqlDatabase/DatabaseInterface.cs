e08ab63bfaec83fd007436124bb3c997ce5e0837
Gregoire D <gregoire.daussin@gmail.com>
2012-02-03 15:12:13 +0100

    Clean database interface

--- SqlDatabase/DatabaseInterface.cs
+++ SqlDatabase/DatabaseInterface.cs
@@ -21,57 +21,19 @@ namespace SqlDatabase
             Option Option = (from r in Entities.Options where r.Name == OptionName select r).FirstOrDefault();
             if (Option == null)
             {
-                Func<KeyValuePair<string,object>,bool> Predicat = o => o.Key == OptionName;
-                KeyValuePair<string,object> DefaultOption = Configuration.DefaultOptions.Where(o => o.Key == OptionName).FirstOrDefault();
-                if(Configuration.DefaultOptions.Count(Predicat) > 0)
-                {
-                    return DefaultValue;
-                }
-
-
-                Option NewOption = new Option();
-                string DefaultType = DefaultValue.GetType().Name;
-                if (DefaultValue is DateTime)
-                {
-                    NewOption.OptionType = GetOptionType(Configuration.DefaultOptionType.DateTime);
-                    NewOption.DateTime = (DateTime)DefaultValue;
-                }
-                else if (DefaultValue is bool)
-                {
-                    NewOption.OptionType = GetOptionType(Configuration.DefaultOptionType.Boolean);
-                    NewOption.Boolean = (bool)DefaultValue;
-                }
-                else if (DefaultValue is decimal)
-                {
-                    NewOption.OptionType = GetOptionType(Configuration.DefaultOptionType.Decimal);
-                    NewOption.Decimal = (decimal)DefaultValue;
-                }
-                else if (DefaultValue is int)
-                {
-                    NewOption.OptionType = GetOptionType(Configuration.DefaultOptionType.Integer);
-                    NewOption.Integer = (int)DefaultValue;
-                }
-                else if (DefaultValue is string)
-                {
-                    NewOption.OptionType = GetOptionType(Configuration.DefaultOptionType.String);
-                    NewOption.String = (string)DefaultValue;
-                }
-                else
-                {
-                    return null;
-                }
-                NewOption.
+                return DefaultValue;
             }
             else
             {
                 switch (Option.OptionType.Name)
                 {
-                    case Configuration.DefaultOptionType.Boolean:  return Option.Boolean;
-                    case Configuration.DefaultOptionType.DateTime: return Option.DateTime;
-                    case Configuration.DefaultOptionType.Decimal: return Option.Decimal;
-                    case Configuration.DefaultOptionType.Integer: return Option.Integer;
-                    case Configuration.DefaultOptionType.String:   return Option.String;
+                    case "Boolean": return Option.Boolean;
+                    case "DateTime": return Option.DateTime;
+                    case "Decimal": return Option.Decimal;
+                    case "Integer": return Option.Integer;
+                    case "String": return Option.String;
                 }
+                return DefaultValue;
             }
         }
         private OptionType GetOptionType(string OptionTypeName)
@@ -86,9 +48,9 @@ namespace SqlDatabase
             return GetOptionType(OptionTypeName);
         }
 
-        public List<Article> GetHighlight()
+        public List<Article> GetHighlightedArticles()
         {
-            return (from r in Entities.Articles where r.Published == true && r.Highlight == true orderby r.CreatedAt descending select r).Take(Configuration.DefaultOptions["NumberOfHighlightedArticles"].Value).ToList();
+            return (from r in Entities.Articles where r.Published == true && r.Highlight == true orderby r.CreatedAt descending select r).Take((int)GetOption("NumberOfHighlightedArticles", 5)).ToList();
         }
     }
 }
